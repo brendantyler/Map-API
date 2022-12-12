ac405137996dd11acdd871dde56f863c038ef982
brendantyler <galmanbrendan@gmail.com>
2022-12-12 12:12:56 -0600

    format header

---
+++ assets/script/index.js
@@ -0,0 +1,9 @@
+
+
+
+/*----------------------------------Map Box Api---------------------------------------*/
+mapboxgl.accessToken = 'pk.eyJ1IjoiYnJlbmRhbmdhbG1hbiIsImEiOiJja3hkaDY3ejMwZDhoMnZwZXByMDV2enQ5In0.hHhzrbbSrPpNQbtmaO2b-g';
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11'
+});
